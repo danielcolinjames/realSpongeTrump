7338666d9d8d10456c35a77bb5d2a9e95fa6d987
Daniel James <danielcolinjames@gmail.com>
2019-01-26 14:42:27 -0800

    Adds basic bot functionality to bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+var Twit = require('twit');
+var TwitterBot = require('node-twitterbot').TwitterBot;
+var Bot = new TwitterBot({
+ consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+var phraseArray = [ "testing 1",
+                    "testing 2",
+                    "testing 3",
+                    "testing 4",
+                    "testing 5",
+                    "testing 6",
+                    "testing 7",
+                    "testing 8",
+                    "testing 9",
+                    "testing 10!" ];
+function chooseRandom(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)];
+}
+var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
+Bot.tweet(phrase);
